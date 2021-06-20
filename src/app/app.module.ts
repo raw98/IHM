b6b6336dee654153f265fa0d3c29c8b12e963e0d
raw98 <78865860+raw98@users.noreply.github.com>
2021-06-20 08:18:37 +0200

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { RechercheComponent } from './recherche/recherche.component';
+import { VendeurComponent } from './vendeur/vendeur.component';
+import { SigninComponent } from './signin/signin.component';
+import { CompteComponent } from './compte/compte.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'acceuil', component:AppComponent},
+  { path: 'recherche', component: RechercheComponent },
+  { path: 'vendeur', component:VendeurComponent},
+  { path: 'signin', component: SigninComponent },
+  { path: 'compte', component: CompteComponent},
+] ;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcceuilComponent,
+    RechercheComponent,
+    VendeurComponent,
+    SigninComponent,
+    CompteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/vendeur/vendeur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vendeur',
+  templateUrl: './vendeur.component.html',
+  styleUrls: ['./vendeur.component.scss']
+})
+export class VendeurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
